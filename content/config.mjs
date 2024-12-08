3aaba0fd5be5eb917342708707839c12435a5824
Brittany Wilson <107086465+zongxi1115@users.noreply.github.com>
2024-12-08 13:28:20 +0800

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,12 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+export default {
+  title:"学习随记",
+  description:"Use flowershow and do a lot for study!",
+  author:"zongxi",
+  domain:"zongxi.xyz",
+  defaultAuthor:"zongxi",
+  showSidebar:true,
+  
+};
